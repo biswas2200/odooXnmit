005a73bbae2a2fc584ccef2942b3d66130b60c5b
biswas2200 <122800409+biswas2200@users.noreply.github.com>
2025-09-06 11:42:01 +0530

    first commit

---
+++ odoo/src/main/java/com/odoo/odoo/model/User.java
@@ -0,0 +1,67 @@
+package com.odoo.odoo.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(unique = true, nullable = false)
+        private String email;
+
+        @Column(nullable = false)
+        private String password;
+
+        @Column(nullable = false)
+        private String username;
+
+        private String fullName;
+        private String phone;
+        private String profileImage;
+
+        @Builder.Default
+        private BigDecimal totalCarbonSaved = BigDecimal.ZERO;
+
+        @Builder.Default
+        private Integer sustainabilityPoints = 0;
+
+        @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
+        @Builder.Default
+        private List<Product> productsListed = new ArrayList<>();
+
+        @OneToMany(mappedBy = "buyer", cascade = CascadeType.ALL)
+        @Builder.Default
+        private List<Order> orders = new ArrayList<>();
+
+        @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+        private Cart cart;
+
+        @CreatedDate
+        private LocalDateTime createdAt;
+
+        @LastModifiedDate
+        private LocalDateTime updatedAt;
+    }
+}
